47bff73efb92d751366b1468ae9a6f2bdd9d3aea
drewbrokke <drew.brokke@liferay.com>
2015-10-03 11:33:58 -0700

    adds user account sign-in

--- client/js/main.js
+++ client/js/main.js
@@ -2,16 +2,18 @@ Meteor.subscribe('contacts');
 Contacts = new Mongo.Collection("contacts");
 
 
-if (Meteor.isClient) {
-	Template.body.helpers({
-		contacts: function () {
-			return Contacts.find({});
-		}
-	});
+Template.body.helpers({
+	contacts: function () {
+		return Contacts.find({});
+	}
+});
 
-	Template.button.events({
-		'click button': function () {
-			Meteor.call('sendEmail');
-		}
-	});
-}
+Template.button.events({
+	'click button': function () {
+		Meteor.call('sendEmail');
+	}
+});
+
+Accounts.ui.config({
+	passwordSignupFields: 'USERNAME_AND_EMAIL'
+});
