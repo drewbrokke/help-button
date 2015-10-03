5f3a7cfb368bfa4dc79275c6b182a7e1c01508c9
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2015-10-03 10:17:22 -0700

    Initial commit for contacts

--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,14 @@
+Meteor.subscribe('contacts');
+Contacts = new Mongo.Collection("contacts");
+
+
 if (Meteor.isClient) {
+	Template.body.helpers({
+		contacts: function () {
+			return Contacts.find({});
+		}
+	});
+
 	Template.button.events({
 		'click button': function () {
 			Meteor.call('sendEmail');
--- server/help-button.js
+++ server/help-button.js
@@ -1,10 +1,16 @@
+Contacts = new Mongo.Collection("contacts");
+
 Meteor.startup(function () {
 	smtp = {
 		username: 'helpbuttondemo',   // eg: server@gentlenode.com
 		password: 'helpbuttondemocftk',   // eg: 3eeP1gtizk5eziohfervU
 		server:   'smtp.gmail.com',  // eg: mail.gandi.net
 		port: 465
-	}
+	};
 
 	process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+});
+
+Meteor.publish("contacts", function () {
+	return Contacts.find({});
 });
